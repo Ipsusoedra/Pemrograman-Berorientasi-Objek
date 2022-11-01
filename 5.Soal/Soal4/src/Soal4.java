ca5ff38301e916ceb2ba68a5fa349ec8bd5851cc
Ipsusoedra <94087164+Ipsusoedra@users.noreply.github.com>
2022-11-01 13:36:17 +0700

    UTS PBO Selasa, 01 November 2022

---
+++ 5.Soal/Soal4/src/Soal4.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo Ideapad 5
+ */
+public class Soal4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (var counter= 0; counter <= 100; counter+=2 ){
+        
+        System.out.println("Perulangan Ganjil : " + counter);
+        }
+    }
+    
+}
