79f9d51be7f08a50f33d6f89874eab212697ff10
Ipsusoedra <94087164+Ipsusoedra@users.noreply.github.com>
2022-11-22 07:26:45 +0700

    Add files via upload

---
+++ 7.JDBC/DatabaseServices/src/com/atep/barang.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.atep;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Lenovo Ideapad 5
+ */
+public class barang {
+    private Connection koneksi;
+ public barang() {
+ koneksi = KoneksiDatabase.getKoneksi();
+ }
+ public void insert(String kd, String nm, int jml, long hrg){
+ Statement statement = null;
+ try {
+ statement =koneksi.createStatement();
+ System.out.println("Statement berhasil dibuat");
+ String url= "INSERT INTO barang(kode_barang,nama_barang,jumlah,harga)VALUES('"+kd+"','"+nm+"',"+jml+","+hrg+")";
+ statement.executeUpdate(url);
+ }catch(SQLException ex){
+ System.out.println("Statement gagal dibuat");
+ System.out.println("Pesan : "+ex.getMessage());
+ }finally{
+ if (statement != null){
+ try{
+ statement.close();
+ System.out.println("Statement berhasil ditutup");
+ }catch(SQLException ex){
+ System.out.println("Statement gagal ditutup");
+ System.out.println("Pesan : "+ex.getMessage());
+ }
+ }
+ }
+ }
+ public void update(String kd, String nm, int jml, long hrg){
+ Statement statement = null;
+ try {
+ statement =koneksi.createStatement();
+ System.out.println("Statement berhasil dibuat");
+ String url="UPDATE barang SET nama_barang='"+nm+"',jumlah="+jml+",harga="+hrg+" WHERE kode_barang='"+kd+"'";
+ statement.executeUpdate(url);
+ }catch(SQLException ex){
+ System.out.println("Statement gagal dibuat");
+ System.out.println("Pesan : "+ex.getMessage());
+ }finally{
+ if (statement != null){
+ try{
+ statement.close();
+ System.out.println("Statement berhasil ditutup");
+ }catch(SQLException ex){
+ System.out.println("Statement gagal ditutup");
+ System.out.println("Pesan : "+ex.getMessage());
+ }
+ }
+ }
+ }
+ public void delete(String kd){
+ Statement statement = null;
+ try {
+ statement =koneksi.createStatement();
+ System.out.println("Statement berhasil dibuat");
+ String url="DELETE FROM barang WHERE kode_barang='"+kd+"'";
+ statement.executeUpdate(url);
+ }catch(SQLException ex){
+ System.out.println("Statement gagal dibuat");
+ System.out.println("Pesan : "+ex.getMessage());
+ }finally{
+ if (statement != null){
+ try{
+ statement.close();
+ System.out.println("Statement berhasil ditutup");
+ }catch(SQLException ex){
+ System.out.println("Statement gagal ditutup");
+ System.out.println("Pesan : "+ex.getMessage());
+ }
+ }
+ }
+ }
+
+}
